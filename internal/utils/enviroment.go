481717b5c227b6f5ce2f595be5a79a4264327f1b
hsmyc <hllsmtyc@gmail.com>
2024-05-24 18:28:27 +0200

    Port problem

--- internal/utils/enviroment.go
+++ internal/utils/enviroment.go
@@ -2,15 +2,18 @@ package utils
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func GetStrapi() (string, string) {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		err := godotenv.Load("../.env")
+		if err != nil {
+			fmt.Println("Error loading .env file")
+		}
 	}
 	strapiUrl := os.Getenv("STRAPI_URL")
 	strapiToken := os.Getenv("STRAPI_TOKEN")
@@ -18,10 +21,12 @@ func GetStrapi() (string, string) {
 }
 
 func GetPort() string {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		if err := godotenv.Load("../.env"); err != nil {
+			log.Fatal("error loading .env file:", err)
+		}
 	}
+
 	port := os.Getenv("PORT")
 	return port
 }
