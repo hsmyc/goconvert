482c3ddee7883bb269789c7f863d5e229e9aad03
hsmyc <hllsmtyc@gmail.com>
2024-05-24 18:16:35 +0200

    Port Settings

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	port := utils.GetPort()
 	if port == "" {
-		port = "8080"
+		port = ":8080"
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Test server is running on port %s\n", port)
