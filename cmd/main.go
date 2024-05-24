d7f1294e378edba589e058da801f913ca7cd3c6e
hsmyc <hllsmtyc@gmail.com>
2024-05-24 17:53:05 +0200

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -13,17 +13,13 @@ func main() {
 }
 
 func corsMiddleware(h http.HandlerFunc) http.HandlerFunc {
-	fmt.Println("corsMiddleware")
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", " POST")
-
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		h(w, r)
 	}
 }
